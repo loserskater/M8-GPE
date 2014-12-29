f14f2cb5c6a88754a0b7e7bb76f73e70909996a1
Matt Booth <kryten2k35@ultimarom.com>
2014-12-29 02:11:33 +0000

    Optimise building
    Removing the original file before building and removing the "update" options of 7zip seem to be the best way to go about it

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
-::UltimaROM build script
+::M8-GPE build script
 set /p VERSION=<version.txt
+del M8-GPE-%VERSION%.zip
 tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip META-INF system boot.img
 cd ..
