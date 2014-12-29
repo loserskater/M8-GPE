db491070a4c2f6e320bff9815d812ccef2c4f7f1
Matt Booth <kryten2k35@ultimarom.com>
2014-12-29 02:12:22 +0000

    Oops
    Forgot to remove this in the last commit

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 ::M8-GPE build script
 set /p VERSION=<version.txt
 del M8-GPE-%VERSION%.zip
-tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip META-INF system boot.img
+tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip META-INF system boot.img
 cd ..
