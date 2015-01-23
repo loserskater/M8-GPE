823520b21fc5c17d40ec7d6bc788ede625f503fd
loserskater <thejasonmaxfield@gmail.com>
2015-01-23 00:50:14 -0500

    Update build process
    Get version number whenever build script is run and update version.txt
    for aroma to read during install.

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 ::M8-GPE build script
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set VERSION=%%a%%b%%c)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
+set VERSION=%%a%%b%%c
+echo %%a-%%b-%%c > META-INF\com\google\android\aroma\version.txt
+)
 del M8-GPE-*.zip
 
 tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip aroma META-INF supersu system boot.img
