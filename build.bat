496cf268c0095708532477e47fc78b252dff6fef
loserskater <thejasonmaxfield@gmail.com>
2015-01-22 23:00:05 -0500

    Changelog, build script
    No more version file to keep up with. Now grabs the date when you run
    the script.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 ::M8-GPE build script
-set /p VERSION=<version.txt
-del M8-GPE-%VERSION%.zip
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set VERSION=%%a%%b%%c)
+del M8-GPE-*.zip
 
 tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip aroma META-INF supersu system boot.img
