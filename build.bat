3d17573e8438b86c30b5a660ba63bd5542f6a257
loserskater <thejasonmaxfield@gmail.com>
2015-01-26 08:19:14 -0500

    Change version numbering
    Makes it more readable.

--- build.bat
+++ build.bat
@@ -1,9 +1,7 @@
 @echo off
 ::M8-GPE build script
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
-set VERSION=%%a%%b%%c
-echo %%a-%%b-%%c > META-INF\com\google\android\aroma\version.txt
-)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set VERSION=%%a-%%b-%%c)
 del M8-GPE-*.zip
+echo %VERSION% > META-INF\com\google\android\aroma\version.txt
 
 tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip aroma META-INF supersu system boot.img
