cf931c2ece9fa061f1fed61737e0081f00e1c936
loserskater <thejasonmaxfield@gmail.com>
2015-02-27 15:28:15 -0500

    Add sed and update build script
    Now during build ro.ota.version is read from ota_version.txt and applied
    to all build.props

--- build.bat
+++ build.bat
@@ -1,7 +1,22 @@
 @echo off
 ::M8-GPE build script
+
+::Set variables
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set VERSION=%%a-%%b-%%c)
+set /p otaversion=<ota_version.txt
+for /F "delims=" %%a in ('findstr /i "ro.ota.version" system\build.prop') do (
+    set value=%%a
+)
+
+::Prepare files for build
 del M8-GPE-*.zip
 echo %VERSION% > META-INF\com\google\android\aroma\version.txt
+::Need to move to tools folder to run sed otherwise it won't remove the temp files it creates
+cd tools
+sed.exe s/%value%/%otaversion%/g ..\system\build.prop
+sed.exe s/%value%/%otaversion%/g ..\aroma\carrier\verizon\system\build.prop
+sed.exe s/%value%/%otaversion%/g ..\aroma\carrier\sprint\system\build.prop
+cd ..
 
+::Build
 tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip aroma META-INF supersu system boot.img
