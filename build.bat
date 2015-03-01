c99b64e988b3502c303053c4c0fde317b51af9dd
loserskater <thejasonmaxfield@gmail.com>
2015-02-28 22:43:06 -0500

    Build script fixes

--- build.bat
+++ build.bat
@@ -11,12 +11,9 @@ for /F "delims=" %%a in ('findstr /i "ro.ota.version" system\build.prop') do (
 ::Prepare files for build
 del M8-GPE-*.zip
 echo %VERSION% > META-INF\com\google\android\aroma\version.txt
-::Need to move to tools folder to run sed otherwise it won't remove the temp files it creates
-cd tools
 sed.exe -i s/%value%/%otaversion%/g ..\system\build.prop
 sed.exe -i s/%value%/%otaversion%/g ..\aroma\carrier\verizon\system\build.prop
 sed.exe -i s/%value%/%otaversion%/g ..\aroma\carrier\sprint\system\build.prop
-cd ..
 
 ::Build
 tools\7za.exe a -mx9 -xr@tools/exclusion.txt M8-GPE-%VERSION%.zip aroma META-INF supersu system boot.img
