d5e09d40ec7f8486a308bc7e5844cf79929b209f
Erin Newman <enewman17@gmail.com>
2015-03-06 21:06:09 -0500

    Updater  changes
    Moved and renamed patch_carrier.sh to patch_apk. Navbar mod now using
    patch_apk to inject resources.arc
    Added aroma 2.70 binary with sprint patch, incase we want to revert till
    amarullz merges the patch in 3.0

---
+++ aroma/scripts/patch_apk/patch_apk.sh
@@ -0,0 +1,10 @@
+#!/sbin/sh
+
+fullfile=$1
+fname=$(basename $fullfile)
+
+cd /tmp/$fname
+/tmp/zip -r $fullfile *
+busybox mkdir aligned
+/tmp/zipalign -f 4 $fullfile ./aligned/$fname
+cp ./aligned/$fname $fullfile
